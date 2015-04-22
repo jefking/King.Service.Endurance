93c1ed5543769aaf64d2bf4bacbc5b81b7fa6b80
Jef King <jef@agilebusinesscloud.com>
2015-04-21 19:41:36 -0700

    added nuget

--- King.Service.Endurance/Generation/WorkerRole.cs
+++ King.Service.Endurance/Generation/WorkerRole.cs
@@ -1,14 +1,8 @@
-using System;
-using System.Collections.Generic;
+using Microsoft.WindowsAzure.ServiceRuntime;
 using System.Diagnostics;
-using System.Linq;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.WindowsAzure;
-using Microsoft.WindowsAzure.Diagnostics;
-using Microsoft.WindowsAzure.ServiceRuntime;
-using Microsoft.WindowsAzure.Storage;
 
 namespace Generation
 {

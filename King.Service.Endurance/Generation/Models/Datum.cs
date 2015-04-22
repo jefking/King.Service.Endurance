b34166236eceeb76311cbaf5aec62d89efb04d78
Jef King <jef@agilebusinesscloud.com>
2015-04-22 11:56:28 -0700

    more data

--- King.Service.Endurance/Generation/Models/Datum.cs
+++ King.Service.Endurance/Generation/Models/Datum.cs
@@ -1,15 +1,26 @@
 ﻿namespace Generation.Models
 {
     using Microsoft.WindowsAzure.Storage.Table;
+    using System;
 
     public class Datum : TableEntity
     {
-        public string EveryMs
+        public int EveryMs
         {
             get;
             set;
         }
         public string ServiceName
+        {
+            get;
+            set;
+        }
+        public DateTime LastRun
+        {
+            get;
+            set;
+        }
+        public DateTime Now
         {
             get;
             set;

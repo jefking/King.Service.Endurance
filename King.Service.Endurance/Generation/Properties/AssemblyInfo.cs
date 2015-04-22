20d53de29182a65c774b4a94510165c0f47ef00d
Jef King <jef@agilebusinesscloud.com>
2015-04-21 19:57:31 -0700

    misc

--- King.Service.Endurance/Generation/Properties/AssemblyInfo.cs
+++ King.Service.Endurance/Generation/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("a73c4f48-50d8-41ea-b2aa-327c170150d4")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]

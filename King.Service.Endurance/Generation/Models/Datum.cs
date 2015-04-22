a226c1e11e9d1d2891f2060a1c7c4d6fbdd7c331
Jef King <jef@agilebusinesscloud.com>
2015-04-22 11:33:32 -0700

    start storing data

---
+++ King.Service.Endurance/Generation/Models/Datum.cs
@@ -0,0 +1,18 @@
+﻿namespace Generation.Models
+{
+    using Microsoft.WindowsAzure.Storage.Table;
+
+    public class Datum : TableEntity
+    {
+        public double EveryTotalMilliseconds
+        {
+            get;
+            set;
+        }
+        public string ServiceName
+        {
+            get;
+            set;
+        }
+    }
+}
--- King.Service.Endurance/Generation/Tasks/Collector.cs
+++ King.Service.Endurance/Generation/Tasks/Collector.cs
@@ -1,20 +1,31 @@
 ﻿namespace Generation.Tasks
 {
+    using Generation.Models;
     using King.Azure.Data;
     using King.Service;
+    using System;
 
     public class Collector : RecurringTask
     {
         private readonly ITableStorage table;
+        private readonly int seconds = 0;
 
         public Collector(int seconds, ITableStorage table)
             :base(15, seconds)
         {
+            this.seconds = seconds;
             this.table = table;
         }
 
         public override void Run()
         {
+            var entity = new Datum();
+            entity.PartitionKey = string.Format("{0}", this.seconds);
+            entity.RowKey = DateTime.UtcNow.ToString();
+            entity.EveryTotalMilliseconds = base.Every.TotalMilliseconds;
+            entity.ServiceName = base.ServiceName;
+
+            this.table.InsertOrReplace(entity);
         }
     }
 }

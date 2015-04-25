4bd34d5544eac7f64ba14eac8df840bef48ee83e
Jef King <jef@agilebusinesscloud.com>
2015-04-25 15:45:43 -0700

    codez

--- DataGeneration/Program.cs
+++ DataGeneration/Program.cs
@@ -5,11 +5,8 @@
     using King.Service;
     using Microsoft.Azure.WebJobs;
 
-    // To learn more about Microsoft Azure WebJobs SDK, please see http://go.microsoft.com/fwlink/?LinkID=320976
     class Program
     {
-        // Please set the following connection strings in app.config for this WebJob to run:
-        // AzureWebJobsDashboard and AzureWebJobsStorage
         static void Main()
         {
             var config = new Config()
